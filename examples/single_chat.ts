daef0964a82fcd5c13801060a6e5cfb7814f602a
Carter Snook <cartersnook04@gmail.com>
2021-11-20 21:33:02 -0600

    feat(examples): add repl chat example

---
+++ examples/single_chat.ts
@@ -0,0 +1,39 @@
+import { ModelManager } from "../mod.ts";
+
+const sleep = (t: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(() => resolve(), t));
+
+const manager = new ModelManager();
+
+console.log("Loading conversation model...");
+
+const convoModel = await manager.createConversationModel();
+
+console.log("Creating conversation...");
+
+const convoManager = await convoModel.createConversationManager();
+const convo = await convoManager.createConversation();
+
+await sleep(500);
+
+console.log("Chat started! Type 'exit' to exit.");
+
+while (1) {
+  await sleep(1000);
+
+  const response = prompt("\n >");
+
+  if (response === null) {
+    throw new Error("Expected user response.");
+  }
+
+  if (response.trim() === "exit" || response.trim() === "'exit'" /* lol */) {
+    break;
+  }
+
+  await sleep(500);
+
+  console.log(` < ${await convo.sendMessage(response)}`);
+}
+
+console.log("\nAlright, bye! Thanks for chatting!");
